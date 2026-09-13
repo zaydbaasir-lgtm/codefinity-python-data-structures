534320515e8acff6eed2e5b2f508e07647a1f9cc
zaydbaasir-lgtm <>
2026-09-13 19:22:49 +0000

    Indexed deletion

---
+++ mastering_python_lists/indexed_deletion/main.py
@@ -0,0 +1,9 @@
+travel_wishlist = ["Paris", "Oslo", "Kyoto", "Sydney", "Rome"]
+
+# Write your code below
+del travel_wishlist[0]
+del travel_wishlist[1]
+
+
+# Testing 
+print("Updated list:", travel_wishlist)
