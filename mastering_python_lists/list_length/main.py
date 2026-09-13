7b3f511d7bb2af2fbc4d2a0a72bffb6b12befe2c
zaydbaasir-lgtm <>
2026-09-13 19:08:21 +0000

    List length

---
+++ mastering_python_lists/list_length/main.py
@@ -0,0 +1,14 @@
+travel_wishlist = [
+    "Paris", "Tokyo", "New York", "Sydney", "Rio de Janeiro", 
+    "Cape Town", "Berlin", "Bangkok", "Moscow", "Dubai",
+    "Rome", "Toronto", "Beijing", "Mumbai", "Seoul",
+    "Istanbul", "Mexico City", "Athens", "San Francisco", "Cairo",
+    "Lisbon", "Buenos Aires", "Singapore", "Helsinki", "Vienna",
+    "Stockholm", "Kuala Lumpur", "Los Angeles", "Jakarta", "Madrid"
+]
+
+# Write your code here
+list_length = len(travel_wishlist)
+
+# Testing
+print("list_length:", list_length)
