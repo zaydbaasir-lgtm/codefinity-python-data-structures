1e0f362dc540053d0f644d6cf0f65533f139df30
zaydbaasir-lgtm <>
2026-09-13 19:23:29 +0000

    List removal

---
+++ mastering_python_lists/list_removal/main.py
@@ -0,0 +1,8 @@
+travel_wishlist = ["Paris", "Oslo", "Kyoto", "Sydney"]
+
+# Deleting element
+travel_wishlist.remove("Oslo")
+travel_wishlist.remove("Sydney")
+
+# Testing
+print('Updated list:', travel_wishlist)
